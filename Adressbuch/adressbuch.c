052b5ee2032e80c56eccfdcd62bd233b46c685df
Jakob Klepp <jakob@truh.in>
2013-10-18 11:39:21 +0200

    ein paar implementierungen und änderungen an den prototypen

--- Adressbuch/adressbuch.c
+++ Adressbuch/adressbuch.c
@@ -2,6 +2,64 @@
   Name of file  : adressbuch.c
   Author        : Jakob Klepp <jklepp@student.tgm.ac.at>
   Version       : 20131018.1
-  Description   : 
-                  
+  Description   : Ein Addressbuch mit CLI Menu
+                  unter verwendung von zwei Structs (PERSON, ADDRESS)
+                  Call-By-Referenc und dynamischer Speicherverwaltung
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include 'adressbuch.h'
+
+int main(int argc, char** argv)
+{
+    PERSON **people = NULL;
+    size_t p_size = 0;
+    bool_t stop = false;
+    while(!stop)
+    {
+        printf("Was wollen sie machen?\n");
+    }
+    return EXIT_SUCCESS;
+}
+
+void list(const size_t *p_size, const PERSON **people)
+{
+    size_t i;
+    for(i = 0; i < *p_size; ++i)
+    {
+        if(*(people+i) != NULL 
+        &&  (**(people+i)).fname != NULL 
+        &&  (**(people+i)).sname != NULL)
+        {
+            printf("%s %s\n", *(*people+i).fname, *(*people+i).sname);
+        }
+    }
+}
+
+PERSON* readin(size_t *p_size, PERSON **people)
+{
+    PERSON *person = NULL;
+    person = malloc(sizeof(PERSON));
+    if(person == NULL)
+    {
+        return NULL;
+    }
+    printf("\n");
+
+    return person;
+}
+
+bool_t remove(size_t *p_size, PERSON **people, PERSON *person)
+{
+
+}
+
+PERSON* modify(size_t *p_size, PERSON **people, PERSON *person)
+{
+
+}
+
+bool_t show(const PERSON *person)
+{
+
+}
--- Adressbuch/adressbuch.h
+++ Adressbuch/adressbuch.h
@@ -7,6 +7,7 @@
                   aendern von Personen
                   und zum ausgeben der Adressdaten einer Person
 */
+#include <stdlib.h>
 
 /*! \brief Struktur von Adressdaten
  */
@@ -34,33 +35,49 @@ typedef struct
 /*! \brief listet die Namen der Personen
  *
  * \param 
+ *
+ * \param
  */
-void list(PERSON **people);
+void list(size_t p_size, const PERSON **people);
 
 /*! \brief liest einen neuen Eintrag von der Commanline ein
  *
  * \param
+ *
+ * \param
+ *
+ * \return
  */
-PERSON* readin(PERSON **people);
+PERSON* readin(size_t *p_size, PERSON **people);
 
 /*! \brief entfernt eine Person aus dem Adressbuch
  *
  * \param
  *
  * \param
+ *
+ * \param
+ *
+ * \return
  */
-bool_t remove(PERSON **people, PERSON *person);
+bool_t remove(size_t *p_size, PERSON **people, PERSON *person);
 
 /*! \brief aendert eine Person, fragt Benutzer nach aenderungswuenschen
  *
  * \param
  *
  * \param
+ *
+ * \param
+ *
+ * \return
  */
-PERSON* modify(PERSON **people, PERSON *person);
+PERSON* modify(size_t *p_size, PERSON **people, PERSON *person);
 
 /*! \brief zeigt alle Daten zu einer Person an
  *
  * \param
+ *
+ * \return
  */
-bool_t show(PERSON *person)
+bool_t show(PERSON *person);
