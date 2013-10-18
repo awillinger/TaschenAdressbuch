bc4078eead415e593bc4cece5177be9db9416375
Jakob Klepp <jakob@truh.in>
2013-10-18 09:31:22 +0200

    Kopfkommentare

---
+++ Adressbuch/adressbuch.c
@@ -0,0 +1,7 @@
+/*
+  Name of file  : adressbuch.c
+  Author        : Jakob Klepp <jklepp@student.tgm.ac.at>
+  Version       : 20131018.1
+  Description   : 
+                  
+*/
---
+++ Taschenrechner/taschenrechner.c
@@ -0,0 +1,7 @@
+/*
+  Name of file  : taschenrechner.c
+  Author        : Andreas Willinger <awillinger@student.tgm.ac.at>
+  Version       : 20131018.1
+  Description   : 
+                  
+*/
